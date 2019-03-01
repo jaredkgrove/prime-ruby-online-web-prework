d3b809d9b9b86f8faffb4d6a618ece6d2f91ff46
Jared Grove <jaredkgrove@gmail.com>
2019-03-01 00:11:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,10 +4,10 @@ def prime?(number)
   denominator = 2
   result = number
   until denominator > result
-    if number%denominator == 0
+    result = number / denominator
+    if number%denominator == 0 && 
       prime = FALSE
     end
-    result = number / denominator
     denominator += 1
   end
   prime
